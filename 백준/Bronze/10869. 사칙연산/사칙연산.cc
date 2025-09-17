556eea44ae2d088b4322f6cb57bea42e4f40811f
jeewon514 <jeewon514@gmail.com>
2025-09-17 11:20:53 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.cc
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -1,12 +1,15 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
     int A,B;
-    cin>>A>>B;
-    cout<<A+B<<endl<<A-B<<endl<<A*B<<endl<<A/B<<endl<<A%B<<endl;
+    cin >> A >> B;
+
+    cout << A+B << '\n' << A-B << '\n' << A*B << '\n' << A/B << '\n' << A%B;
+
     return 0;
 }
