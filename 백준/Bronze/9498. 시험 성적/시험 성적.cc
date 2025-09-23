61146495af094d628c855a0e5d63bf4ced3f308c
jeewon514 <jeewon514@gmail.com>
2025-09-23 11:02:43 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    if (n < 60) cout << "F";
+    else if (n < 70) cout << "D";
+    else if (n < 80) cout << "C";
+    else if (n < 90) cout << "B";
+    else cout << "A";
+
+    return 0;
+}
