f3cb7e52eef8c256370b428fef4ebb150b0a4c9c
jeewon514 <jeewon514@gmail.com>
2025-10-07 18:31:59 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.cc
+++ 백준/Bronze/10807. 개수 세기/개수 세기.cc
@@ -1,21 +1,22 @@
-#include <bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
 
 int main() {
-    cin.tie(0)->sync_with_stdio(0);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    int N;cin>>N;
-
-    vector<int> arr(N);
-    for(int i=0;i<N;i++) {
-        cin>>arr[i];
+    int N;cin >> N;
+    int arr[100];
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
     }
+    int v;cin >> v;
 
-    int v;cin>>v;
     int cnt = 0;
     for (int i = 0; i < N; i++) {
-        if (arr[i] == v) cnt++;
-    }cout<<cnt<<'\n';
+        if (arr[i] == v) cnt += 1;
+    } cout << cnt;
 
     return 0;
 }
